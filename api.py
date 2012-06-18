e4cb312bc8b4e4daae866dbb5555d2da00cd16a7
Zuyang Kou <leafduo@gmail.com>
2012-06-18 14:40:39 +0800

    Add user handler.

--- api.py
+++ api.py
@@ -14,3 +14,31 @@ class RootHandler(api_base.BaseHandler):
     @api_base.auth
     def get(self):
         """Print a simple welcome message"""
+
+class UserHandler(api_base.BaseHandler):
+    """User handler, which can create user and modify user profiles.
+    """
+
+    api_path = '/user'
+
+    @api_base.auth
+    @api_base.json
+    def post(self):
+        """Create a new user."""
+
+        if self.get_user_role != 'admin':
+            raise tornado.web.HTTPError(403)
+
+        try:
+            email = self.req['email']
+            first_name = self.req['first_name']
+            last_name = self.req['last_name']
+            role = self.req['role']
+        except KeyError:
+            raise tornado.web.HTTPError(400)
+
+        if role not in ('fellow', 'admin'):
+            raise tornado.web.HTTPError(400)
+
+        self.mongo.user.insert({"_id": email, "first_name": first_name,
+            "last_name": last_name, "role": role})
