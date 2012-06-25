8294b73ff5c128248357f9bf4faffdeb9010be84
Zuyang Kou <leafduo@gmail.com>
2012-06-26 01:52:53 +0800

    Continuing lines using backslash is not recommended.

--- api.py
+++ api.py
@@ -26,22 +26,37 @@ class UserHandler(api_base.BaseHandler):
     """User handler, which can create user and modify user profiles.
     """
 
-    api_path = '/user/([^/]*)'
-    profile_key_modifiable = ('first_name', 'last_name', 'password', 'status', 'gender',  'language',  'work_field',  'location',  'population_target', \
-                               'mobile_countrycode',  'mobile',  'email_type',  'street',  'city',  'province',  'zip',  'country',  'skype_ID', \
-                               'organization_address',  'organization_name',  'organization_acronym',  'organization_formed_date', \
-                               'organization_website',  'organization_type',  'organization_employee_num',  'organization_budget', \
-                               'organization_phone_countrycode',  'organization_phone')
-    profile_key_checkable = ('first_name', 'last_name', 'status', 'role',  'gender',  'language',  'work_field',  'location',  'population_target', \
-                               'mobile_countrycode',  'mobile',  'email_type',  'street',  'city',  'province',  'zip',  'country',  'skype_ID', \
-                               'organization_address',  'organization_name',  'organization_acronym',  'organization_formed_date', \
-                               'organization_website',  'organization_type',  'organization_employee_num',  'organization_budget', \
-                               'organization_phone_countrycode',  'organization_phone')
-    profile_key_enum = {'gender':('male', 'female', 'secrecy'), 'role':('admin', 'fellow'), 'email_type':('home', 'business'),  \
-                        'organization_type':('Private sector','Government Agency','Multilateral'),  \
-                        'organization_employee_num':('less than 10', '11-25', '26-40', '41-60', '61-80', '81-100', '101-150', '151-200', 'more than 200'), \
-                        'organization_budget':('less than $50,000', '$50,000-$100000','$100,000-$200,000','$200,000-$500,000', \
-                        '$500,000-$1,000,000', '$1,000,000-$5,000,000', '$5,000,000-$10,000,000', 'more than $10,000,000')}
+    api_path = 'guser/([^/]*)'
+    profile_key_modifiable = ('first_name', 'last_name', 'password',
+            'status', 'gender',  'language',  'work_field',  'location',
+            'population_target', 'mobile_countrycode',  'mobile',
+            'email_type', 'street', 'city',  'province',  'zip',  'country',
+            'skype_ID', 'organization_address',  'organization_name',
+            'organization_acronym',  'organization_formed_date',
+            'organization_website',  'organization_type',
+            'organization_employee_num',  'organization_budget',
+            'organization_phone_countrycode',  'organization_phone')
+    profile_key_checkable = ('first_name', 'last_name', 'status', 'role',
+            'gender',  'language',  'work_field',  'location',
+            'population_target', 'mobile_countrycode',  'mobile',
+            'email_type',  'street',  'city',  'province',  'zip',
+            'country',  'skype_ID', 'organization_address',
+            'organization_name',  'organization_acronym',
+            'organization_formed_date', 'organization_website',
+            'organization_type',  'organization_employee_num',
+            'organization_budget', 'organization_phone_countrycode',
+            'organization_phone')
+    profile_key_enum = {'gender':('male', 'female', 'secrecy'),
+            'role':('admin', 'fellow'), 'email_type':('home', 'business'),
+            'organization_type':
+            ('Private sector','Government Agency','Multilateral'),
+            'organization_employee_num':
+            ('less than 10', '11-25', '26-40', '41-60', '61-80', '81-100',
+                '101-150', '151-200', 'more than 200'),
+            'organization_budget':('less than $50,000', '$50,000-$100000',
+                '$100,000-$200,000','$200,000-$500,000',
+                '$500,000-$1,000,000', '$1,000,000-$5,000,000',
+                '$5,000,000-$10,000,000', 'more than $10,000,000')}
 
     def restrict_to(self, d, it):
         """delete all items in dictionary except items whose keys in it (iterable)"""
